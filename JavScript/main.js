a406c9e12ac944db8a1e652d99f1eb6a5be57e09
Zachnjue <njuezachary@gmail.com>
2017-08-20 16:12:43 +0300

    Added a JQuery file

--- JavScript/main.js
+++ JavScript/main.js
@@ -1,5 +1,27 @@
-function calc(){
-  var number= document.getElementById("entNumber").value;
+//Front End Code
+$(document).ready(function(){
+  $("#submit1").click(function(event) {
+    event.preventDefault();
+    var userInput=parseInt($("#user_input").val());
+    var resultofArray= pingpongLoop(userInput);
+    $("#display").text(resultofArray);
+    });
+  });
 
-
+//Back End Code
+var pingpongLoop=function(userInput) {
+  var resultofArray = [];
+  for (var i = 1; i < userInput; i+=) {
+    if (((i%3)===0) &&((i%5)===0)){
+      resultofArray.push("pinpong");
+    } else if ((i%3)===0) {
+      resultofArray.push("ping");
+    } else if ((i%5)===0)) {
+      resultofArray.push("pong");
+    } else {
+      resultofArray.push(i);
+    }
+  }
+ return resultofArray;
 }
+*/
